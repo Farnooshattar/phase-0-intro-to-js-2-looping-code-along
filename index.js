df6e10cafb5e41962b6e57e108f1b1635c201643
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-03-06 22:43:26 -0500

    complete all

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(names) {
+  let i = 0; // the initialization moves OUTSIDE the body of the loop!
+  let messages=[]
+  while (i < names.length) {
+    console.log(`Thank you ${names[i]} for the wonderful surprise gift!`);
+    messages[i]=`Thank you, ${names[i]}, for the wonderful surprise gift!`
+    i++; // the iteration moves INSIDE the body of the loop!
+  }
+
+  return messages;
+}
+function countDown(number)
+{
+    while(number>=0)
+{
+    console.log(number);
+        number--;
+}
+        
+
+        
+}
+writeCards(names);
+countDown(11);
